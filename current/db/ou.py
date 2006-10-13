b13716b3c15245695a59480456328723c12c6112
jjneely <jjneely@7112b371-5efc-0310-9db1-efd4ebc68ee9>
2006-10-13 01:56:17 +0000

    Initial SQL to handle the OU trees.  This took a few hours to get teh SQL to work in both MySQL and SQLite.  I don't believe that SQLite is advanced enough to do the node insertion completely in SQL as I need variables.
    These trees are represented in SQL by using the nested sets method which
    takes advantage of the special properties of the pre-order traversal. 
    
    
    git-svn-id: http://current.tigris.org/svn/current/trunk@301 7112b371-5efc-0310-9db1-efd4ebc68ee9

---
+++ current/db/ou.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+#
+# ou.py - Database code for the Org Unit tree
+#
+# Copyright 2006 Jack Neely <jjneely@gmail.com>
+#
+# SDG
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+from types import *
+
+from current.exception import *
+from current import db
+from current.db.resultSet import resultSet
+from current.logger import *
+
+class ProfileDB(object):
+
+    def __init__(self):
+        self.conn   = db.sdb.getConnection()
+        self.cursor = db.sdb.getCursor()
+
+    def getOUID(self, label):
+        q1 = """select count(ou_id) where ou_id = %s"""
+        q2 = """select ou_id where label = %s"""
+
+        if isinstance(label, IntType):
+            self.cursor.execute(q1, (label,))
+            r = self.cursor.fetchone()
+            if r[0] > 0:
+                return label
+            else:
+                return None
+        else:
+            self.cusor.execute(q2, (label,))
+            r = self.cursor.fetchone()
+            if self.cursor.rowcount() == 0:
+                return None
+            else:
+                return self.cursor.fetchone()[0]
+
+    def insertOU(self, parent, label, desc):
+        """Insert an OU that will be a child of the given parent OU."""
+        # XXX: This can be done entirely in SQL if SQLite can do variables
+        # like @varname := foo
+
+        if self.getOUID(parent) == None:
+            raise CurrentOUError("Invalid OU ID.")
+
+        # select the rgt value of the parent
+        self.cursor.execute("select rgt from OU where ou_id = %s", (parent,))
+        prgt = self.cursor.fetchone()[0]
+
+        # add 2 to  all lft/rgt values >= prgt
+        q1 = """update OU set lft = lft + 2 where lft >= %s"""
+        q2 = """update OU set rgt = rgt + 2 where rgt >= %s"""
+        self.cursor.execute(q1, (prgt,))
+        self.cursor.execute(q2, (prgt,))
+
+        # create new nodes lft/rgt values
+        lft = prgt
+        rgt = lft + 1
+
+        q = """insert into OU (label, description, lft, rgt) values
+               (%s, %s, %s, %s)"""
+        self.cursor.execute(q, (label, desc, lft, rgt))
+
+        q = """select ou_id from OU where lft = %s and rgt = %s"""
+        self.cursor.execute(q, (lft, rgt))
+        ret = self.cursor.fetchone()[0]
+
+        self.conn.commit()
+
+        return ret
+
+    def isChild(self, parent, child):
+        """Returns True if child is in the subtree rooted at parent."""
+
+        parent = self.getOUID(parent)
+        child = self.getOUID(child)
+
+        if parent == None:
+            raise CurrentOUError("Invalid OU ID for parent.")
+        if child == None:
+            raise CurrentOUError("Invalid OU ID for child.")
+
+        q = """select count(*) from OU as node, OU as parent w
+                  where parent.ou_id = %s 
+                  and node.ou_id = %s
+                  and node.lft between parent.lft and parent.rgt"""
+
+        self.cursor.execute(q, (parent, child))
+        return self.cursor.fetchone()[0] > 0
+
+    def subTree(self, root):
+        root = self.getOUID(root)
+        if root == None:
+            raise CurrentOUError("Invalid OU ID.")
+
+        q = """select node.label, node.ou_id,
+                  (count(parent.label) - (sub_tree.depth + 1)) as depth
+               from OU as node,
+                  OU as parent,
+                  OU as sub_parent,
+                  (   select node.ou_id as ou_id, 
+                      (count(parent.label) - 1) as depth
+                      from OU as node, OU as parent
+                      where node.lft between parent.lft and parent.rgt
+                      and node.ou_id = %s
+                      group by node.label
+                      order by node.lft
+                  ) as sub_tree
+               where node.lft between parent.lft and parent.rgt
+               and node.lft between sub_parent.lft and sub_parent.rgt
+               and sub_parent.ou_id = sub_tree.ou_id
+               group by node.label
+               order by node.lft"""
+
+        self.cursor.execute(q, (root,))
+        return resultSet(self.cursor).dump()
